06f159d5a53894b9e3bc6f55fe071d0f615716c6
Dave Sutherland <dave@daveography.ca>
2020-06-04 14:19:57 -0600

    Simplify Permit model

--- src/app/models/permit/permit.ts
+++ src/app/models/permit/permit.ts
@@ -1,113 +1,18 @@
-import { PermitId } from './permit-id';
-import { PermitZoning } from './permit-zoning';
-import { PermitAddress } from './permit-address';
-import { PermitNeighbourhood } from './permit-neighbourhood';
-import { Position } from 'src/app/models/common/geo/position';
-import { PermitDescription } from './permit-description';
-import { PermitSubtype } from './permit-subtype';
-import { PermitClass } from './permit-class';
-import { PermitCategory } from './permit-category';
-import { PermitStatus } from './permit-status';
-import { PermitValue } from './permit-value';
 import { PermitType } from './permit-type';
 
-export class Permit {
-  readonly Id: PermitId;
-  readonly Date: Date;
-  readonly Position: Position;
-  readonly Zoning: PermitZoning;
-  readonly Address: PermitAddress;
-  readonly Neighbourhood: PermitNeighbourhood;
-  readonly Description: PermitDescription;
-  readonly Type: PermitType;
-  readonly Subtype: PermitSubtype;
-  readonly Class: PermitClass;
-  readonly Category: PermitCategory;
-  readonly Status: PermitStatus;
-  readonly Value: PermitValue;
-
-  constructor(private _id: PermitId,
-    private _date: Date,
-    private _position: Position,
-    private _zoning: PermitZoning,
-    private _address: PermitAddress,
-    private _neighbourhood: PermitNeighbourhood,
-    private _description: PermitDescription,
-    private _type: PermitType,
-    private _subtype: PermitSubtype,
-    private _class: PermitClass,
-    private _category: PermitCategory,
-    private _status: PermitStatus,
-    private _value: PermitValue) {
-
-      if (!_id) {
-        throw new Error('Id must be provided');
-      }
-
-      if (!_date) {
-        throw new Error('Date must be provided');
-      }
-
-      if (!_position) {
-        throw new Error('Position must be provided');
-      }
-
-      if (!_zoning) {
-        throw new Error('Zoning must be provided');
-      }
-
-      if (!_address) {
-        throw new Error('Address must be provided');
-      }
-
-      if (!_neighbourhood) {
-        throw new Error('Neighbourhood must be provided');
-      }
-
-      if (!_description) {
-        throw new Error('Description must be provided');
-      }
-
-      if (!_type) {
-        throw new Error('Type must be provided');
-      }
-
-      if (!_subtype) {
-        throw new Error('Subtype must be provided');
-      }
-
-      if (!_class) {
-        throw new Error('Class must be provided');
-      }
-
-      if (!_category) {
-        throw new Error('Category must be provided');
-      }
-
-      if (!_status) {
-        throw new Error('Status must be provided');
-      }
-
-      if (!_value) {
-        throw new Error('Value must be provided');
-      }
-
-      this.Id = _id;
-      this.Date = _date;
-      this.Position = _position;
-      this.Zoning = _zoning;
-      this.Address = _address;
-      this.Neighbourhood = _neighbourhood;
-      this.Description = _description;
-      this.Type = _type;
-      this.Subtype = _subtype;
-      this.Class = _class;
-      this.Category = _category;
-      this.Status = _status;
-      this.Value = _value;
-    }
-
-  public toString(): string {
-    return this.Description.toString();
-  }
+export interface Permit {
+  Id: string;
+  Date: Date;
+  Latitude: number;
+  Longitude: number;
+  Zoning: string;
+  Address: string;
+  Neighbourhood: string;
+  Description: string;
+  Type: PermitType;
+  Subtype: string;
+  Class: string;
+  Category: string;
+  Status: string;
+  Value: number;
 }
--- src/app/services/permit/odp-to-permit-service.ts
+++ src/app/services/permit/odp-to-permit-service.ts
@@ -1,21 +1,8 @@
+import { Injectable } from '@angular/core';
+import { BuildingPermit } from 'src/app/models/edmonton-open-data/building-permit';
 import { DevelopmentPermit } from 'src/app/models/edmonton-open-data/development-permit';
 import { Permit } from 'src/app/models/permit/permit';
-import { Position } from 'src/app/models/common/geo/position';
-import { Latitude } from 'src/app/models/common/geo/latitude';
-import { Longitude } from 'src/app/models/common/geo/longitude';
-import { PermitZoning } from 'src/app/models/permit/permit-zoning';
-import { PermitAddress } from 'src/app/models/permit/permit-address';
-import { PermitNeighbourhood } from 'src/app/models/permit/permit-neighbourhood';
-import { PermitDescription } from 'src/app/models/permit/permit-description';
-import { PermitClass } from 'src/app/models/permit/permit-class';
-import { PermitValue } from 'src/app/models/permit/permit-value';
-import { PermitId } from 'src/app/models/permit/permit-id';
 import { PermitType } from 'src/app/models/permit/permit-type';
-import { PermitSubtype } from 'src/app/models/permit/permit-subtype';
-import { PermitStatus } from 'src/app/models/permit/permit-status';
-import { PermitCategory } from 'src/app/models/permit/permit-category';
-import { BuildingPermit } from 'src/app/models/edmonton-open-data/building-permit';
-import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
@@ -23,42 +10,40 @@ import { Injectable } from '@angular/core';
 export class OdpToPermitService {
 
   public FromDevelopmentPermit(developmentPermit: DevelopmentPermit): Permit {
-    return new Permit(
-      new PermitId(developmentPermit.city_file_number),
-      new Date(developmentPermit.permit_date),
-      new Position(
-        new Latitude(developmentPermit.latitude),
-        new Longitude(developmentPermit.longitude)),
-      new PermitZoning(developmentPermit.zoning),
-      new PermitAddress(developmentPermit.address),
-      new PermitNeighbourhood(developmentPermit.neighbourhood),
-      new PermitDescription(developmentPermit.description_of_development),
-      PermitType.DevelopmentPermit,
-      new PermitSubtype(developmentPermit.permit_type),
-      new PermitClass(developmentPermit.permit_class),
-      new PermitCategory('N/A'),
-      new PermitStatus(developmentPermit.status),
-      new PermitValue(0)
-    );
-  };
+    return {
+      Id: developmentPermit.city_file_number,
+      Date: new Date(developmentPermit.permit_date),
+      Latitude: developmentPermit.latitude,
+      Longitude: developmentPermit.longitude,
+      Zoning: developmentPermit.zoning,
+      Address: developmentPermit.address,
+      Neighbourhood: developmentPermit.neighbourhood,
+      Description: developmentPermit.description_of_development,
+      Type: PermitType.DevelopmentPermit,
+      Subtype: developmentPermit.permit_type,
+      Class: developmentPermit.permit_class,
+      Category: 'N/A',
+      Status: developmentPermit.status,
+      Value: 0
+    };
+  }
 
   public FromBuildingPermit(buildingPermit: BuildingPermit): Permit {
-    return new Permit(
-      new PermitId(buildingPermit.row_id),
-      new Date(buildingPermit.issue_date),
-      new Position(
-        new Latitude(buildingPermit.latitude),
-        new Longitude(buildingPermit.longitude)),
-      new PermitZoning(buildingPermit.zoning),
-      new PermitAddress(buildingPermit.address),
-      new PermitNeighbourhood(buildingPermit.neighbourhood),
-      new PermitDescription(buildingPermit.job_description),
-      PermitType.BuildingPermit,
-      new PermitSubtype(buildingPermit.work_type),
-      new PermitClass(buildingPermit.building_type),
-      new PermitCategory(buildingPermit.job_category),
-      new PermitStatus('Issued'),
-      new PermitValue(buildingPermit.construction_value)
-    );
-  };
+    return {
+      Id: buildingPermit.row_id,
+      Date: new Date(buildingPermit.issue_date),
+      Latitude: buildingPermit.latitude,
+      Longitude: buildingPermit.longitude,
+      Zoning: buildingPermit.zoning,
+      Address: buildingPermit.address,
+      Neighbourhood: buildingPermit.neighbourhood,
+      Description: buildingPermit.job_description,
+      Type: PermitType.BuildingPermit,
+      Subtype: buildingPermit.work_type,
+      Class: buildingPermit.building_type,
+      Category: buildingPermit.job_category,
+      Status: 'Issued',
+      Value: buildingPermit.construction_value
+    };
+  }
 }
